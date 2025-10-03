a39f7bceb69ff70816fb3102a7f5e7bb75edc78f
Ajay Kadwal <ajaykadwal.work@gmail.com>
2025-10-03 12:26:16 +0530

    Initial Commit

---
+++ Use_CoreData/Use_CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Use_CoreDataApp.swift
+//  Use_CoreData
+//
+//  Created by AJAY KADWAL on 03/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Use_CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
